0789b9a613c0b85a1f53afd2c93aeb07d5009e77
AlexWeinreb <alexis.weinreb@yale.edu>
2021-08-18 19:08:44 -0400

    Errors in format_collapsed_exons: adding metadata, correcting NAs...

--- R/format_collapsed_exons.R
+++ R/format_collapsed_exons.R
@@ -14,23 +14,46 @@ new_exons_db <- readRDS("intermediates/210819_new_db_as_granges.rds")
 
 
 #~ Build metadata columns ----
+mcols(new_exons_db)$Name <- mcols(new_exons_db)$exon_name
+
 mcols(new_exons_db)$transcript_name <- mcols(new_exons_db)$exon_name %>% 
   stringr::str_match("^([A-Za-z0-9.+_]+)\\.e[0-9]+$") %>%
   (function(x) x[,2])()
 
 
-mcols(new_exons_db)$gene_id <- mcols(new_exons_db)$transcript_name %>%
-  stringr::str_match("^([A-Za-z0-9_]+\\.[t0-9]+)[a-z.+0-9]*$") %>%
-  (function(x) x[,2])() %>%
-  wb_seq2id(gids, warn_missing = TRUE)
+roworder <- mcols(new_exons_db)$transcript_name %>%
+  stringr::str_match("^([A-Za-z0-9_]+\\.t{0,1}[0-9]+)[a-z.+0-9]*$") %>%
+  magrittr::extract(,2) %>%
+  match(gids$sequence)
 
-mcols(new_exons_db)$gene_symbol <- mcols(new_exons_db)$transcript_name %>%
-  stringr::str_match("^([A-Z0-9]+\\.[t0-9]+)[a-z.+0-9]*$") %>%
-  (function(x) x[,2])() %>%
-  wb_seq2name(gids, warn = TRUE)
+
+mcols(new_exons_db)$gene_id <- gids$gene_id[roworder]
+mcols(new_exons_db)$gene_symbol <- gids$symbol[roworder]
+
+
+
+
+
+
+
+
+# Create ids for the new exons and transcripts
+exid <- mcols(new_exons_db)$exon_id
+exid[is.na(exid)] <- seq(from = 1+max(exid, na.rm = TRUE),
+                         to = (max(exid, na.rm = TRUE) + length(which(is.na(exid)))))
+mcols(new_exons_db)$exon_id <- exid
+
+mcols(new_exons_db)$transcript_id <- as.integer(as.factor(mcols(new_exons_db)$transcript_name))
+
+# finalize metadata columns
+mcols(new_exons_db)$type <- 'exon'
+
+mcols(new_exons_db)$source <- 'WS277_collapsed'
+genome(new_exons_db) <- "WS277_collapsed"
 
 
 col_txdb <- makeTxDbFromGRanges(new_exons_db)
+
 
 
 
@@ -45,7 +68,6 @@ cat("\nNumber of genes: ")
 mcols(new_exons_db)$gene_id %>%
   unique() %>%
   length()
-cat("\n vs initial number of genes: ",length(genelist),"\n")
 
 
 #~ Export ----
