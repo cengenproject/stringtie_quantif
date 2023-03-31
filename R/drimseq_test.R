33ec18087acabaae8bb9077ad0704a3316d877ab
AlexWeinreb <alexis.weinreb@yale.edu>
2023-03-31 16:37:24 -0400

    DRIMSeq adapted to new dataset 2023-03 to look for DTU in ASER and AVA

--- R/drimseq_test.R
+++ R/drimseq_test.R
@@ -6,12 +6,19 @@ library(tidyverse)
 library(DRIMSeq)
 library(wbData)
 tx2g_tab <- wb_load_tx2gene(281)
+gids <- wb_load_gene_ids(281)
+
+fitdms <- qs::qread("data/intermediates_drimseq/2023-03-30_drimseq_fitdms.qs")
 
 
-fitdms <- readRDS("data/intermediates_drimseq/2022-05-03_fitdms.rds")
-
-
-
+# counts for filtering
+counts_per_gene <- counts(fitdms) |>
+  pivot_longer(-c(gene_id, feature_id),
+               names_to = "sample_id",
+               values_to = "count") |>
+  mutate(neuron_id = str_match(sample_id, "^([A-Zef0-9]{2,4})r[0-9]{1,4}$")[,2]) |>
+  group_by(neuron_id, gene_id) |>
+  summarize(count = sum(count))
 
 
 # Prepare contrasts ----
@@ -25,17 +32,72 @@ contr_neurs <- map_chr(factors_in_design,
                                  paste0(factors_in_design |> setdiff(.x),
                                         collapse = "+"),
                                  ") / ",
-                                 length(factors_in_design) - 1))
+                                 length(factors_in_design) - 1)) |>
+  set_names(str_match(factors_in_design, "^neuron_id([[:alnum:]]{2,4})$")[,2])
 
 
-# Test ----
 
-tdms_full <- dmTest(fitdms, contrast= my_contrasts, verbose = 1)
+# Test head neurons ----
+
+# ALA not sequenced
+
+# ASER
+tdms_ASER <- dmTest(fitdms, contrast = limma::makeContrasts(contr_neurs[["ASER"]], levels = design_no_int))
 
 
-my_contrasts <- limma::makeContrasts(contr_neurs[1], levels=design_no_int)
-tdms_AFD <- dmTest(fitdms, contrast= my_contrasts)
-table(results(tdms_AFD)$adj_pvalue < 0.05)
+table(results(tdms_ASER)$adj_pvalue < 0.1, useNA = "ifany")
+
+results(tdms_ASER) |> head()
+
+results(tdms_ASER) |>
+  filter(adj_pvalue < 0.1) |>
+  mutate(gene_name = i2s(gene_id, gids, warn_missing = TRUE)) |>
+  arrange(desc(lr)) |>
+  as_tibble()
+
+genes_expr_in_ASER <- counts_per_gene |>
+  filter(neuron_id == "ASER") |> 
+  select(-neuron_id)
+
+
+results(tdms_ASER) |>
+  filter(adj_pvalue < 0.1) |>
+  mutate(gene_name = i2s(gene_id, gids, warn_missing = TRUE)) |>
+  arrange(desc(lr)) |>
+  as_tibble() |>
+  left_join(genes_expr_in_ASER, by = "gene_id") |>
+  clipr::write_clip()
+
+
+# AVA
+
+tdms_AVA <- dmTest(fitdms, contrast = limma::makeContrasts(contr_neurs[["AVA"]], levels = design_no_int))
+
+
+table(results(tdms_AVA)$adj_pvalue < 0.1, useNA = "ifany")
+
+results(tdms_AVA) |> head()
+
+genes_expr_in_AVA <- counts_per_gene |>
+  filter(neuron_id == "AVA") |> 
+  select(-neuron_id)
+
+
+results(tdms_AVA) |>
+  filter(adj_pvalue < 0.1) |>
+  mutate(gene_name = i2s(gene_id, gids, warn_missing = TRUE)) |>
+  arrange(desc(lr)) |>
+  as_tibble() |>
+  left_join(genes_expr_in_AVA, by = "gene_id") |>
+  filter(count > 2000) |>
+  clipr::write_clip()
+
+
+
+
+
+## Old code below this point ----
+
 
 
 
